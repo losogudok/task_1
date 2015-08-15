337aac245e59a02c2df9e00d7f186bf99a38dfbc
andrey <losogudok@yandex.ru>
2015-08-15 23:22:09 +0300

    format code

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,21 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('watch', function(){
-	gulp.watch('./scss/**/*.scss', function(event){
+gulp.task('watch', function() {
+	gulp.watch('./scss/**/*.scss', function(event) {
 		gulp.src(event.path)
 			.pipe(sass({outputStyle: 'expanded'}))
 			.pipe(gulp.dest('./css'));
 	});
 });
 
-
-gulp.task('compile', function(){
+gulp.task('compile', function() {
 	gulp.src(['scss/*.scss'])
-			.pipe(sass({outputStyle: 'expanded'}))
-			.pipe(autoprefixer({
-				browsers:  ['last 2 versions']
-			}))
-			.pipe(gulp.dest('./css'));
+		.pipe(sass({outputStyle: 'expanded'}))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+		}))
+		.pipe(gulp.dest('./css'));
 });
 
 
